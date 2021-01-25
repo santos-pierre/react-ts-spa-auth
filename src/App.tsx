c6bc44e81c2fe104d778a17d98a47b681d5e1be3
Pierre Santos <santospierre1305@gmail.com>
2021-01-25 16:29:03 +0100

    API Client + user client

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-
 const App = () => {
-    return <h1>Hello World</h1>;
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
 };
 
 export default App;
